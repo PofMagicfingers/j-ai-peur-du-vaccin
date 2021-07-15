20ff897ca3a00c6f5da6a90af0476a105126caad
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2021-07-15 22:13:35 +0200

    feat: Extrait les Sources en composant

---
+++ src/components/badge.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Badge = styled.span`
+  color: #fff;
+  background-color: #088413;
+  border: 1px solid #088413;
+  font-size: 12px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  border-radius: 4px;
+  padding: 2px 4px;
+  display: inline-block;
+  position: relative;
+  top: -2px;
+  margin-left: 10px;
+  line-height: 1;
+`;
+
+export default Badge;
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,9 @@ import { lighten } from "polished";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import Badge from "../components/badge";
+import Sources from "../components/sources";
+import Separator from "../components/separator";
 
 // styles
 const pageStyles = {
@@ -34,18 +37,12 @@ const listStyles = {
   paddingLeft: 0,
 };
 
-const StyledLi = styled.li`
+const StyledQuestion = styled.h2`
   font-size: 21px;
-  max-width: 720px;
   margin-bottom: 30px;
-  font-weight: 300;
+  font-weight: 700;
+  color: ${({ color, theme }) => (theme.id !== 'dark' ? color : lighten(0.5, color))};
 `;
-
-const linkStyle = {
-  fontWeight: "bold",
-  fontSize: 19,
-  verticalAlign: "5%",
-};
 
 const StyledParagraph = styled.p`
   color: ${({ theme }) => theme.text};
@@ -55,31 +52,9 @@ const StyledParagraph = styled.p`
   line-height: 1.5em;
 `;
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 12,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "2px 4px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-};
-
-const sourceStyle = {
-  marginTop: 10,
-};
-
-const sourceAuthorStyle = {
-  marginTop: 4,
-  marginLeft: 4,
-  fontSize: 15,
-};
+const StyledDescription = styled(StyledParagraph)`
+  font-weight: 300;
+`;
 
 const autresResStyle = {
   marginBottom: 72,
@@ -93,9 +68,6 @@ const questions = [
     description:
       "Les vaccins COVID, bien qu'ayant des effets secondaires, présentent moins de risques d'effets sur la santé que le COVID, peu importe son âge ou le vaccin utilisé.",
     color: "#993B00",
-    StyledLi: styled(StyledLi)`
-      color: ${({ theme }) => lighten(theme.id == "dark" ? 0.5 : 0, "#993B00")};
-    `,
     sources: [
       {
         text: "Page interactive de calcul balance bénéfice risques",
@@ -114,9 +86,6 @@ const questions = [
     description:
       "C'est vrai, néanmoins il réduit énormément la charge virale, c'est-à-dire le nombre de copies du virus dans l'organisme. Cela réduit fortement les chances de contracter une forme symptomatique ou grave de la maladie, et quasiment à zéro les risques de transmissions à une autre personne vaccinée ou non.",
     color: "#305154",
-    StyledLi: styled(StyledLi)`
-      color: ${({ theme }) => lighten(theme.id == "dark" ? 0.5 : 0, "#305154")};
-    `,
     sources: [
       {
         text: "A quel point la vaccination freine-t-elle la transmission du COVID ?",
@@ -130,9 +99,6 @@ const questions = [
     description:
       "Les compositions des vaccins COVID sont librement accessibles sur Internet. C'est leur procédé de fabrication qui reste un secret industriel.",
     color: "#A2026C",
-    StyledLi: styled(StyledLi)`
-      color: ${({ theme }) => lighten(theme.id == "dark" ? 0.5 : 0, "#A2026C")};
-    `,
     sources: [
       {
         text: "Composition des vaccins COVID (Pfizer, Moderna, Astrazeneca...)",
@@ -145,9 +111,6 @@ const questions = [
     description:
       "Nous n'avons effectivement pas de recul à long terme sur le procédé, mais c'est aussi le cas de quasiment tous les vaccins et médicaments au moment de leur mise sur le marché. Les effets secondaires à long terme sont évalués après la mise sur le marché d'un médicament ou d'un vaccin. Une surveillance longue est mise en place une fois le vaccin/médicament distribué à large échelle. Par ailleurs, la technologie des vaccins à ARN messager est en étude depuis près de 30 ans et nous avons une certaine connaissance de son fonctionnement, même si nous n'avons que quelques vaccins sur le marché, produits avec ce procédé depuis 2018, et d'autres en phase d'essai (vaccins antirabiques, antigrippaux et anticancéreux notamment).",
     color: "#0B5A8E",
-    StyledLi: styled(StyledLi)`
-      color: ${({ theme }) => lighten(theme.id == "dark" ? 0.5 : 0, "#0B5A8E")};
-    `,
     sources: [
       {
         text: "Page Wikipédia des vaccins à ARNm",
@@ -160,9 +123,6 @@ const questions = [
     description:
       "Pour répondre à la crise du COVID-19, de nombreux labos, donateurs, et gouvernements ont débloqué énormément de ressources financières pour la recherche d'un vaccin. Des essais cliniques accélérés ont également été possibles grâce à un nombre de volontaires exceptionnellement haut par rapport à des essais cliniques habituelles. Le nombre de volontaires, la forte implication de toute la communauté scientifique et le déblocage de financements massifs ont permis la sortie rapide des vaccins.",
     color: "#4D5C00",
-    StyledLi: styled(StyledLi)`
-      color: ${({ theme }) => lighten(theme.id == "dark" ? 0.5 : 0, "#4D5C00")};
-    `,
     sources: [
       {
         text: "Les raisons d'un vaccin en temps record",
@@ -215,10 +175,10 @@ const IndexPage = () => {
       <SEO title={title} />
 
       <h1 style={headingStyles}>
-        J'ai peur du vaccin COVID
-        <br />
+        J'ai peur du vaccin COVID<br />
         <HeadingAccent>Il est normal et sain d'avoir des doutes</HeadingAccent>
       </h1>
+
       <StyledParagraph style={paragraphBlockStyles}>
         Dans le climat de crise actuel, la multiplication des sources
         d'informations, souvent contradictoires, pas claires, ou parfois fausses
@@ -226,71 +186,49 @@ const IndexPage = () => {
         ressources apportant des réponses scientifiques à des questions
         légitimes.
       </StyledParagraph>
-      <ul style={listStyles}>
-        {questions.map((q) => (
-          <q.StyledLi key={q.text}>
-            <span>
-              <h5>
-                {q.text}
-                {q.badge && (
-                  <span style={badgeStyle} aria-label="Nouvelle ressource">
-                    Nouveau !
-                  </span>
-                )}
-              </h5>
-              <StyledParagraph>{q.description}</StyledParagraph>
-              <h5>Source{q.sources?.length > 1 && "s"} :</h5>
-              <ul>
-                {q.sources.map((s) => (
-                  <li key={`${s.text}_${s.url}`} style={sourceStyle}>
-                    <a style={linkStyle} href={s.url} target="_blank">
-                      {s.text}
-                    </a>
-                    {s.author && (
-                      <div style={sourceAuthorStyle}>
-                        <span>{s.author}</span>
-                      </div>
-                    )}
-                  </li>
-                ))}
-              </ul>
-            </span>
-          </q.StyledLi>
+
+      <section style={listStyles}>
+        {questions.map(question => (
+          <React.Fragment key={question.text}>
+            <Separator />
+
+            <StyledQuestion color={question.color}>
+              «&nbsp;{question.text}&nbsp;»
+              {question.badge && <Badge />}
+            </StyledQuestion>
+
+            <StyledDescription>
+              {question.description}
+            </StyledDescription>
+
+            <Sources
+              color={question.color}
+              content={question.sources}
+              title={`Source${question.sources?.length > 1 ? 's' : ''}`}
+            />
+          </React.Fragment>
         ))}
-      </ul>
+      </section>
+
       <section style={autresResStyle}>
-        <h4>Autres ressources</h4>
+        <h2>Autres ressources</h2>
+
         <StyledParagraph>
           Les liens suivants sont des ressources, pages web, articles, vidéos
           qui regroupent des réponses à plusieurs des questions cités
           précédemment, ou qui partagent leur vision plus généraliste sur la
           vaccination.
         </StyledParagraph>
-        <ul>
-          {autresRessources.map((s) => (
-            <li key={`other_${s.url}`} style={sourceStyle}>
-              <a style={linkStyle} href={s.url} target="_blank">
-                {s.text}
-              </a>
-                {s.badge && (
-                  <span style={badgeStyle} aria-label="Nouvelle ressource">
-                    Nouveau !
-                  </span>
-                )}
-              {s.author && (
-                <div style={sourceAuthorStyle}>
-                  <span>{s.author}</span>
-                </div>
-              )}
-            </li>
-          ))}
-        </ul>
+
+        <Sources content={autresRessources} />
       </section>
+
       <section style={paragraphBlockStyles}>
-        <h4>Où se faire vacciner en France ?</h4>
+        <h2>Où se faire vacciner en France ?</h2>
+
         <StyledParagraph>
           Vous pouvez trouver un lieu de vaccination grâce au service{" "}
-          <a href="https://vitemadose.covidtracker.fr/" target="_blank">
+          <a href="https://vitemadose.covidtracker.fr/" target="_blank" rel="noopener">
             Vite Ma Dose
           </a>{" "}
           qui permet de rechercher simultanément un RDV sur toutes les
@@ -302,28 +240,33 @@ const IndexPage = () => {
           <a
             href="https://www.ameli.fr/assure/covid-19/vaccination-contre-la-covid-19/qui-peut-se-faire-vacciner-quand-et-comment"
             target="_blank"
+            rel="noopener"
           >
             Qui peut se faire vacciner, quand et comment ?
           </a>
         </StyledParagraph>
       </section>
+
       <section style={paragraphBlockStyles}>
-        <h4>À propos</h4>
+        <h2>À propos</h2>
+
         <StyledParagraph>
           Ce site est apolitique : il n'a aucun lien avec un parti politique
           et/ou le gouvernement de quelque pays que ce soit.
         </StyledParagraph>
+
         <StyledParagraph>
           En cas d'erreur, de problème, ou si vous pensez qu'une ressource
           manque, n'hésitez pas à participer à ce site via le{" "}
           <a
             href="https://github.com/PofMagicfingers/j-ai-peur-du-vaccin"
             target="_blank"
+            rel="noopener"
           >
             projet GitHub
           </a>{" "}
           ou en envoyant un mail à{" "}
-          <a href="mailto:contact@jaipeurduvaccin.fr" target="_blank">
+          <a href="mailto:contact@jaipeurduvaccin.fr" target="_blank" rel="noopener">
             contact@jaipeurduvaccin.fr
           </a>
         </StyledParagraph>
